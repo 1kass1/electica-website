a5698c35b18cdcfc00813b140087c27aeeb25712
Konstantin <141934405+1kass1@users.noreply.github.com>
2024-05-22 21:13:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+$(function(){
+    $('.leftArrow').on('click', function() {
+        $('#gallery').removeClass('values').addClass('about');
+    });
+    $('.rightArrow').on('click', function() {
+        $('#gallery').removeClass('about').addClass('values');
+    });
+
+    $('#leftReviewArrow').on('click', function(event) {
+        if ($(event.target).hasClass('inactive')) {
+            return;
+        }
+        const reviewsContainer = document.getElementById('reviews');
+        const offset = reviewsContainer.querySelector('.reviewContainer').offsetWidth;
+        reviewsContainer.scrollLeft -= offset + 30;
+        $('#rightReviewArrow').removeClass('inactive');
+    });
+
+    $('#rightReviewArrow').on('click', function(event) {
+        if ($(event.target).hasClass('inactive')) {
+            return;
+        }
+        const reviewsContainer = document.getElementById('reviews');
+        const offset = reviewsContainer.querySelector('.reviewContainer').offsetWidth;
+        reviewsContainer.scrollLeft += offset + 30;
+        $('#leftReviewArrow').removeClass('inactive');
+    });
+});
